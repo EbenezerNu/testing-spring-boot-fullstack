f22c7755991998f56241ed133906cfdc095889a2
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2023-03-15 19:14:25 +0000

    testing errors and exceptions in the student service

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -13,6 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -61,6 +66,29 @@ class StudentServiceTest {
         verify(studentRepository).save(studentArgumentCaptor.capture());
 
         assertThat(student).isEqualTo(studentArgumentCaptor.getValue());
+
+    }
+
+    @Test
+    void cannotAddStudent() {
+        // given
+        Student student = new Student(
+                "Catalyst",
+                "catalyst@test.com",
+                Gender.MALE
+        );
+
+        // when
+        given(studentRepository.selectExistsEmail(any()))
+                .willReturn(true);
+
+        // then
+       assertThatThrownBy(() -> underTest.addStudent(student))
+               .hasMessageContaining("Email " + student.getEmail() + " taken")
+               .isInstanceOf(BadRequestException.class);
+
+        verify(studentRepository, never())
+                .save(any());
 
     }
 
