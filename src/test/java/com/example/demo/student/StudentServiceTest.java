2464028e26bc7532bdc1cfcf2509e4b632020209
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2023-03-15 18:56:55 +0000

    used ExtendWith MockExtension and ArgumentCaptor

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,31 +4,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
     private StudentRepository studentRepository;
-    private AutoCloseable autoCloseable;
+//    private AutoCloseable autoCloseable;
     private StudentService underTest;
 
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
+//        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
     }
 
-    @AfterEach
+    /*@AfterEach
     void tearDown() throws Exception {
        autoCloseable.close();
-    }
+    }*/
 
     @Test
     void getAllStudents() {
@@ -39,8 +44,24 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
     void addStudent() {
+        // given
+        Student student = new Student(
+                "Catalyst",
+                "catalyst@test.com",
+                Gender.MALE
+        );
+
+        // when
+        underTest.addStudent(student);
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        assertThat(student).isEqualTo(studentArgumentCaptor.getValue());
+
     }
 
     @Test
